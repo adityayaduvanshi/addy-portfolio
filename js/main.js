f39fdaf78e754acae9d6c72cf963cd152441d106
Aditya Yaduvanshi <38696088+adityayaduvanshi@users.noreply.github.com>
2022-04-12 23:07:53 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,25 +44,7 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
   el.addEventListener("click", toggleSkills);
 });
-/*==================== QUALIFICATION TABS ====================*/
-// const tabs = document.querySelectorAll("[data-target]"),
-//   tabContents = document.querySelectorAll("[data-content]");
 
-// tabs.forEach((tab) => {
-//   tab.addEventListener("click", () => {
-//     const target = document.querySelectorAll(tab.dataset.target);
-
-//     tabContents.forEach((tabContents) => {
-//       tabContents.classList.remove("qualification__active");
-//     });
-//     target.classList.add("qualification__active");
-
-//     tabs.forEach((tab) => {
-//       tab.classList.remove("qualification__active");
-//     });
-//     tab.classList.add("qualification__active");
-//   });
-// });
 
 /*==================== SERVICES MODAL ====================*/
 
@@ -99,7 +81,7 @@ let swiper = new Swiper(".portfolio__container", {
     clickable: true,
   },
 });
-/*==================== TESTIMONIAL ====================*/
+
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll("section[id]");
